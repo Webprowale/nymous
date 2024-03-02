d3deb00f2e4cddb99e60d54206f63b9e4c1a9f5e
Webprowale <webprowale@gmail.com>
2024-03-02 12:50:49 -1100

    Initial Commit

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -13,21 +13,18 @@ function Register() {
           <span className="fs-1 text-black">🧸</span>
           <h2 className="fw-bold  fs-2 fText mb-5">Register</h2>
         </div>
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="mb-3 inputBody  ps-3 mx-auto">
             <span>
               <FontAwesomeIcon icon={faUser} size="2xl" color="#C33764" />
             </span>
             <input
               type="text"
-              value={getUser.username}
-              onChange={handleChange}
+              
               placeholder="Username"
               name="username"
             />
           </div>
-           <>{errors.username && <p className="error">{errors.username}</p>}
-        </>
           <div className="  inputBody  ps-3 mx-auto">
             <span>
               <FontAwesomeIcon icon={faLock} size="2xl" color="#C33764" />
@@ -35,13 +32,10 @@ function Register() {
             <input
               type="password"
               placeholder="Password"
-              value={getUser.password}
-              onChange={handleChange}
+              
               name="password"
             />
           </div>
-          <>{errors.username && <p className="error">{errors.username}</p>}
-        </>
           <button
             type="submit"
             className="formBtn hero text-white px-5 py-2 rounded-pill w-100"
